3c8dd316a90a55583e03dac1aa6a04f61fd4f275
makhoninrost8-ai <makhoninrost8@gmail.com>
2026-08-27 17:21:31 +0500

    leetcode

---
+++ Easy/0383_Ransom_Note.py
@@ -0,0 +1,29 @@
+"""
+1.Ransom Note
+Difficulty: Easy
+Link:https://leetcode.com/problems/ransom-note/
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+Pattern: Hash Map
+"""
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        letters={}
+        for letter in magazine:
+            if letter in letters:
+                letters[letter] += 1
+            else:
+                letters[letter] = 1
+        for second_letter in ransomNote:
+            if second_letter in letters and letters[second_letter] > 0:
+                letters[second_letter] -= 1
+            else:
+                return False
+        return True
+                
+       
+        
+      
