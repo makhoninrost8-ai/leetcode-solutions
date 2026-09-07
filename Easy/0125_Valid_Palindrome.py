5321ab64901104468187ab9db59f016e7fcce09f
makhoninrost8-ai <makhoninrost8@gmail.com>
2026-09-07 16:46:32 +0500

    Update

---
+++ Easy/0125_Valid_Palindrome.py
@@ -0,0 +1,26 @@
+"""
+1.  Valid Palindrome
+Difficulty: Easy
+Link:https://leetcode.com/problems/valid-palindrome
+
+Time Complexity: O(N)
+Space Complexity: O(1)
+Pattern: Two Pointers
+"""
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s)-1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left +=1
+            while left < right and not s[right].isalnum():  
+                right -=1
+            if s[left].lower() !=s[right].lower():
+                return False
+            left +=1
+            right -=1
+        return True            
+
+
